5923cba8e0400942048d0bf79516497095e179ed
TTYB12 <ttang12@ucsc.edu>
2020-11-01 07:34:20 -0800

    added Header cmpnt

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AngularFireModule } from '@angular/fire';
 
+import { MatToolbarModule } from '@angular/material/toolbar';
+
 import { environment } from '../environments/environment';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { HeaderComponent } from './header/header.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    AngularFireModule.initializeApp(environment.firebase)
+    AngularFireModule.initializeApp(environment.firebase),
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
