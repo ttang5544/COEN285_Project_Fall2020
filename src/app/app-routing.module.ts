0391a6566685adae6868c36c7643cb79fcfb1776
ttang5544 <ttang2@scu.edu>
2020-12-01 00:08:46 -0800

    first combined main frontend

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,22 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
 import { PostCreateComponent } from './posts/post-create/post-create.component';
 import { PostListComponent } from './posts/post-list/post-list.component';
-import { ItemFormComponent } from './item-form/item-form.component';
-
 import { MessagepageComponent } from './messagepage/messagepage.component';
-
-import { WelcomeComponent } from './welcome/welcome.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
 import { LoginComponent } from './login/login.component';
-import { SignupComponent } from './signup/signup.component';
-
 import { WelcomeComponent } from './welcome/welcome.component';
-
 import { SigninComponent } from './signin/signin.component';
 import { SignupComponent } from './signup/signup.component';
-
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 
@@ -28,50 +18,12 @@ const routes: Routes = [
   { path: 'signup', component: SignupComponent},
   { path: 'post-create-path', component: PostCreateComponent  },
   { path: 'post-list-path', component: PostListComponent },
-  { path: 'item-form-component', component: ItemFormComponent },
   { path: 'app-messagepage-component', component: MessagepageComponent }
 ];
-
-// const routes: Routes = [
-//   {
-//     path: '', redirectTo: 'home', pathMatch: 'full'
-//   },
-//   {
-//     path: 'home', component: AppComponent,
-//     children: [
-//       {
-//         path: '', redirectTo: 'login', pathMatch: 'full'
-//       },
-//       {
-//         path: 'login', component: WelcomeComponent
-//       }
-//     ]
-//   },
-//   {
-//     path: 'dashboard', component: DashboardComponent,
-//     children: [
-//       {
-//         path: 'post-create-path', component: PostCreateComponent
-//       },
-//       {
-//         path: 'post-list-path', component: PostListComponent
-//       },
-//       {
-//         path: 'item-form-component', component: ItemFormComponent
-//       }
-//     ]
-//   },
-//   {
-//     path: 'signin-path', component:SigninComponent
-//   },
-//   {
-//     path: 'signup-path', component:SignupComponent
-//   }
-// ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
-export const routingComponents = [PostCreateComponent, PostListComponent, ItemFormComponent];
+export const routingComponents = [PostCreateComponent, PostListComponent];
