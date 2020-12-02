50879d5b5dbd04fd68711d5cef526c25a15e7c36
jerry <>
2020-12-01 23:38:15 -0800

    Added snippet to hide password

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -8,6 +8,7 @@ import {FormControl, Validators} from '@angular/forms';
 })
 export class LoginComponent implements OnInit {
   email = new FormControl('', [Validators.required, Validators.email]);
+  hide = true;
 
   getErrorMessage() {
     if (this.email.hasError('required')) {
--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+//import { userAuth } from '../users.service';
 
 @Component({
   selector: 'app-signup',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
+  firstname: string;
+  lastname: string;
+  email: string;
+  password: string;
+  hide = true;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  signUp() {
+    //userAuth.registerWithEmail(this.email,this.password,this.firstname,this.lastname);
+    console.log(this.email+this.password+this.firstname+this.lastname)
   }
 
 }
